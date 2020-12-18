350f4c4aee9394036dab222c351821b0fe880bb9
mohamad_sdg <mohamad.r.sadeghi93@gmail.com>
2020-12-19 00:02:51 +0330

    shift to auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { UserRepository } from './repository/user.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private readonly userRepository: UserRepository,
+  ) {}
+  public signUp(authCredential: AuthCredentialsDto) {
+    return this.userRepository.signUp(authCredential);
+  }
+  public signIn(authCredential: AuthCredentialsDto) {
+    return this.userRepository.signIn(authCredential);
+  }
+}
