6c583dbe6d1bb47ad75aaddffb70dad1715d577b
mohamad_sdg <mohamad.r.sadeghi93@gmail.com>
2020-12-19 21:00:45 +0330

    setup relation

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,5 +1,15 @@
-import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, Unique, UpdateDateColumn } from "typeorm";
-import { compare } from "bcrypt";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+import { compare } from 'bcrypt';
+import { Task } from 'src/tasks/entities/task.entity';
 @Entity()
 @Unique(['username'])
 export class User extends BaseEntity {
@@ -18,13 +28,20 @@ export class User extends BaseEntity {
   @Column()
   salt: string;
 
+  @OneToMany(
+    () => Task,
+    task => task.user,
+    { eager: true },
+  )
+  tasks: Task[];
+
   @CreateDateColumn({ name: 'created_at' })
   'created_at': Date;
 
   @UpdateDateColumn({ name: 'updated_at' })
   'updated_at': Date;
 
-  public async validatePassword(password:string){
+  public async validatePassword(password: string) {
     return await compare(password, this.password);
   }
 }
--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,27 +1,44 @@
-import { BaseEntity, Column, CreateDateColumn, Entity, Generated, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-import { TaskStatus } from "../task.model";
+import { User } from 'src/auth/entities/user.entity';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { TaskStatus } from '../task.model';
 
 @Entity()
-export class Task extends BaseEntity{
-    @PrimaryGeneratedColumn()
-    id:number;
+export class Task extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    @Generated("uuid")
-    uuid: string;
+  @Column()
+  @Generated('uuid')
+  uuid: string;
 
-    @Column()
-    title:string;
+  @Column()
+  title: string;
 
-    @Column()
-    description:string;
+  @Column()
+  description: string;
 
-    @Column({type:'enum',enum:TaskStatus,default:TaskStatus.OPEN})
-    status:TaskStatus
+  @Column({ type: 'enum', enum: TaskStatus, default: TaskStatus.OPEN })
+  status: TaskStatus;
 
-    @CreateDateColumn({ name: 'created_at' })
-    'created_at': Date;
+  @ManyToOne(
+    () => User,
+    user => user.tasks,
+    { eager: false },
+  )
+  user: User;
 
-    @UpdateDateColumn({ name: 'updated_at' })
-    'updated_at': Date;
+  @CreateDateColumn({ name: 'created_at' })
+  'created_at': Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  'updated_at': Date;
 }
